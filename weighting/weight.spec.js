4d26eec0fcd2bfc880d553ef8475caee64d49cf5
Ross-mc <rossmci90@hotmail.co.uk>
2022-01-25 16:23:58 +0000

    added function to produce a weighted array

--- weighting/weight.spec.js
+++ weighting/weight.spec.js
@@ -1,29 +1,44 @@
-const { produceWeightedWords } = require("./weighting");
+const { produceWeightedWords, produceArrayWithWordsMultipledByWeight } = require("./weighting");
 
 const mockWords = ["hello", "valid", "words"];
 
 describe("weighting.js", () => {
-  it("produceWeightedWords should return an array of equal length to the original array", () => {
+  it("produceWeightedWords should return an object with all the words present", () => {
     const result = produceWeightedWords(mockWords);
-    expect(result).toHaveLength(3);
+    expect(result).toHaveProperty('hello');
+    expect(result).toHaveProperty('valid');
+    expect(result).toHaveProperty('words');
   });
 
-  it("produceWeightedWords should return an array with each word weighted according to the letters present", () => {
+  it("produceWeightedWords should return an object with each word weighted according to the letters present", () => {
     const result = produceWeightedWords(mockWords);
-    const expected = [
-      {
+    const expected = {
+      hello: {
         word: "hello",
         weight: 343,
       },
-      {
+      valid :{
         word: "valid",
         weight: 245,
       },
-      {
+      words:{
         word: "words",
         weight: 265,
       },
-    ];
+    };
     expect(result).toEqual(expected);
+  });
+
+  it("produceArrayWithWordsMultipledByWeight should return an array with all the values multiplied", () => {
+    const weightedWords = produceWeightedWords(mockWords);
+    const result = produceArrayWithWordsMultipledByWeight(mockWords, weightedWords)
+    expect(result).toHaveLength(853);
+    expect(result[0]).toBe('hello');
+    expect(result[50]).toBe('hello');
+    expect(result[342]).toBe('hello');
+    expect(result[343]).toBe('valid');
+    expect(result[587]).toBe('valid');
+    expect(result[588]).toBe('words');
+    expect(result[852]).toBe('words');
   });
 });
--- weighting/weighting.js
+++ weighting/weighting.js
@@ -1,7 +1,14 @@
 const { letterWeights } = require("./weights");
 
 const produceWeightedWords = (wordsArr) => {
-  return wordsArr.map(weightWord);
+  return wordsArr.reduce((weightedWordObj, word) => {
+    return {
+      ...weightedWordObj,
+      [word]: {
+        ...weightWord(word)
+      }
+    }
+  },{});
 };
 
 const weightWord = (word) => {
@@ -15,6 +22,18 @@ const weightWord = (word) => {
   };
 };
 
+const produceArrayWithWordsMultipledByWeight = (currentWords, weightedWords) => {
+  const multipliedWords = [];
+  for (const word of currentWords){
+    const {weight} = weightedWords[word];
+    const arr = Array(weight);
+    arr.fill(word);
+    multipliedWords.push(...arr);
+  }
+  return multipliedWords
+}
+
 module.exports = {
   produceWeightedWords,
+  produceArrayWithWordsMultipledByWeight
 };
